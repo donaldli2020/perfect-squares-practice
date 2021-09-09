7b16bd59c86efeb6e0702288dd76fd9c92d65908
donaldli2020 <donaldli2020@outlook.com>
2021-09-09 19:43:46 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,8 +1,8 @@
 var answer;
 alert('Return to homepage when finished.');
 for (var i = 1; i < Infinity; i = i + 1) {
-  answer = prompt('What is ' + i + ' squared?');
-  if (parseInt(answer.value) === '${i ** 2}') {
+  answer = parseFloat(prompt('What is ' + i + ' squared?'));
+  if (answer.value === '${i ** 2}') {
     alert('Correct!');
   } else {
     alert('Sorry, the answer is ' + i ** 2);
