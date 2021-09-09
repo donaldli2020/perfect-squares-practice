8e89ac85418aa6324fe19ad0fcafcae755995963
donaldli2020 <donaldli2020@outlook.com>
2021-09-09 19:52:06 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -3,7 +3,7 @@ alert('Return to homepage when finished.');
 for (var i = 1; i < Infinity; i = i + 1) {
   answer = prompt('What is ' + i + ' squared?');
   answer = answer * 1
-  if (answer.value === i ** 2) {
+  if (answer === i ** 2) {
     alert('Correct!');
   } else {
     alert('Sorry, the answer is ' + i ** 2);
