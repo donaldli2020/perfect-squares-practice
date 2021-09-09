3827b9d3fc045d6ad07ec92112b7753b5687187e
donaldli2020 <donaldli2020@outlook.com>
2021-09-09 12:31:07 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,9 @@
+var answer;
+for (var i = 1; i < Infinity; i = i + 1) {
+  answer = prompt('What's ' + i + 'squared?');
+  if (answer.value = "${i ** 2}") {
+    alert('Correct!');
+  } else {
+    alert('Sorry, the answer is ' + i ** 2);
+  }
+}
